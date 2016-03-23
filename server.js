914bf0f43198688ffb228e4ab8456f9d0a532a28
andrewdudek84 <andrewdudek84@gmail.com>
2016-03-23 12:21:10 -0400

    changes

---
+++ server.js
@@ -0,0 +1,27 @@
+﻿var HTTP = require('http'),
+    URL = require('url'),
+    Assert = require('assert'),
+    Port = process.env.PORT || 1337;
+    User = require('./Modules/Models/User');
+
+HTTP.createServer(function (req, res) {
+ 
+    var resStr = "_p(" + JSON.stringify({ message: "Not a valid request!" }) + ")";
+
+    if (req.url.indexOf("Request") != -1) {
+        
+        var param = JSON.parse(URL.parse(req.url, true).query.request);
+        
+        new User(function (args) {
+
+            var resStr = "_p(" + JSON.stringify({ data: [args.User] }) + ")";
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(resStr);
+
+        }).Get(param.UserID);       
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(resStr);    
+       
+}).listen(Port);
