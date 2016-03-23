0fb5fc644b702ad9171f792da086fcbb5fac5e39
andrewdudek84 <andrewdudek84@gmail.com>
2016-03-23 12:25:30 -0400

    Changes

--- server.js
+++ server.js
@@ -6,22 +6,20 @@
 
 HTTP.createServer(function (req, res) {
  
-    var resStr = "_p(" + JSON.stringify({ message: "Not a valid request!" }) + ")";
-
     if (req.url.indexOf("Request") != -1) {
         
         var param = JSON.parse(URL.parse(req.url, true).query.request);
         
         new User(function (args) {
-
+            
             var resStr = "_p(" + JSON.stringify({ data: [args.User] }) + ")";
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(resStr);
 
-        }).Get(param.UserID);       
-    }
-    
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(resStr);    
+        }).Get(param.UserID);
+    } else {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end("_p(" + JSON.stringify({ message: "Not a valid request!" }) + ")");
+    }  
        
 }).listen(Port);
